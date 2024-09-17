d729c7f1f821ff65b6bba6cafad48eb6a4df55c4
hamasfaa <hamasfa@gmail.com>
2024-09-18 04:56:16 +0700

    first commit

---
+++ src/components/field.vue
@@ -0,0 +1,7 @@
+<template>
+  <!-- <img src="../assets/Lapangan.png" alt="" srcset="" /> -->
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup>
+import Field from "../components/field.vue";
+</script>
+
+<template>
+  <main><Field /></main>
+</template>
