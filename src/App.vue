af4bce87d931cd5af8357dec5350c69cb847210c
hamasfaa <hamasfa@gmail.com>
2024-09-18 16:50:58 +0700

    pinia

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,46 @@
 <template>
-  <!-- <router-view> </router-view> -->
   <div>
     <input type="text" v-model="message" />
     <button @click="sendMessage">Send</button>
+
+    <p>Received message: {{ receivedMessage }}</p>
   </div>
 </template>
 
 <script>
-import ROSLIB from "roslib";
 import { useRobotStore } from "./stores/store";
 
 export default {
+  setup() {
+    const robotStore = useRobotStore();
+
+    return {
+      robotStore,
+    };
+  },
+
   data() {
     return {
-      ros: null,
-      talker: null,
       message: "",
     };
   },
-  created() {
-    this.ros = new ROSLIB.Ros({
-      url: "ws://localhost:9090",
-    });
-
-    this.ros.on("connection", () => {
-      console.log("Connected to websocket server.");
-    });
-
-    this.ros.on("error", (error) => {
-      console.log("Error connecting to websocket server: ", error);
-    });
-
-    this.ros.on("close", () => {
-      console.log("Connection to websocket server closed.");
-    });
-
-    this.listener = new ROSLIB.Topic({
-      ros: this.ros,
-      name: "/pc2bs",
-      messageType: "std_msgs/String",
-    });
-
-    this.listener.subscribe((message) => {
-      console.log(
-        "Received message on " + this.listener.name + ": " + message.data
-      );
-    });
-
-    this.talker = new ROSLIB.Topic({
-      ros: this.ros,
-      name: "/bs2pc",
-      messageType: "std_msgs/String",
-    });
+  computed: {
+    receivedMessage() {
+      return this.robotStore.receivedMessage;
+    },
   },
   methods: {
     sendMessage() {
-      var str = new ROSLIB.Message({
-        data: this.message,
-      });
-      this.talker.publish(str);
+      this.robotStore.updateMessage(this.message);
+      this.robotStore.sendMessage();
+    },
+  },
+  created() {
+    this.robotStore.initializeROS();
+  },
+  watch: {
+    message(newValue) {
+      this.robotStore.updateMessage(newValue);
     },
   },
 };
--- src/stores/store.js
+++ src/stores/store.js
@@ -1,15 +1,66 @@
 // import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import ROSLIB from 'roslib';
 
 export const useRobotStore = defineStore("robot", {
-    state: () => {
-        return {
-            bs2pc: "",
-        };
-    },
+    state: () => ({
+        ros: null,
+        bs2pc: null,
+        pc2bs: null,
+        message: "",
+        receivedMessage: "",
+    }),
+
     actions: {
-        setbs2pc(bs2pc) {
-            this.bs2pc = "kosong";
+        initializeROS() {
+            this.ros = new ROSLIB.Ros({
+                url: "ws://localhost:9090",
+            });
+
+            this.ros.on("connection", () => {
+                console.log("Connected to websocket server.");
+            });
+
+            this.ros.on("error", (error) => {
+                console.log("Error connecting to websocket server: ", error);
+            });
+
+            this.ros.on("close", () => {
+                console.log("Connection to websocket server closed.");
+            });
+
+            //buat subscribe
+            this.pc2bs = new ROSLIB.Topic({
+                ros: this.ros,
+                name: "/pc2bs",
+                messageType: "std_msgs/String",
+            });
+
+            // buat publish
+            this.bs2pc = new ROSLIB.Topic({
+                ros: this.ros,
+                name: "/bs2pc",
+                messageType: "std_msgs/String",
+            });
+
+            this.pc2bs.subscribe((message) => {
+                this.receivedMessage = message.data;
+                console.log('Received message on ' + this.pc2bs.name + ': ' + message.data);
+            });
         },
+
+        sendMessage() {
+            if (this.bs2pc) {
+                const message = new ROSLIB.Message({
+                    data: this.message
+                });
+                this.bs2pc.publish(message);
+                console.log('Sending message on ' + this.bs2pc.name + ': ' + message.data);
+            }
+        },
+
+        updateMessage(newMessage) {
+            this.message = newMessage
+        }
     },
 });
